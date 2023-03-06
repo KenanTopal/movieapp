07121f53acec98b28b55b92663d8648dbc07015f
KadirTopal <kadirkenantopal@gmail.com>
2023-03-06 13:23:42 -0500

    added changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-
-import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import MovieDetails from './pages/MovieDetails'
-import Register from './pages/Register/Register'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import AuthProvider from "./context/AuthContext";
+import MovieProvider from "./context/MoviesContext";
 
 function App() {
-  return (
-    <AuthProvider>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/details/:id" element={<MovieDetails/>} />
-      </Routes>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<MovieProvider>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/details/:id" element={<MovieDetails />} />
+				</Routes>
+			</MovieProvider>
+		</AuthProvider>
+	);
 }
 
 export default App;
